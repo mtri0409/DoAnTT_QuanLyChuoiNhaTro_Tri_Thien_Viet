b4765d0c4ef1cef0eef23b9d4744750dc0850690
minhtri <minhtri05k47@gmail.com>
2026-03-21 19:29:34 +0700

    first commit

---
+++ backend/qlchuoiphongtro/src/main/java/com/trithienviet/qlchuoiphongtro/QlchuoiphongtroApplication.java
@@ -0,0 +1,13 @@
+package com.trithienviet.qlchuoiphongtro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlchuoiphongtroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlchuoiphongtroApplication.class, args);
+	}
+
+}
