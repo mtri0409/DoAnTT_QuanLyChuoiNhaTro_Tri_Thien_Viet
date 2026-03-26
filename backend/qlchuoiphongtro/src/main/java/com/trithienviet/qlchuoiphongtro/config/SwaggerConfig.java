209065ea35eb35439a0f402a57c980428930b7aa
minhtri <minhtri05k47@gmail.com>
2026-03-26 17:25:52 +0700

    feat-be: update swagger auth

--- backend/qlchuoiphongtro/src/main/java/com/trithienviet/qlchuoiphongtro/config/SwaggerConfig.java
+++ backend/qlchuoiphongtro/src/main/java/com/trithienviet/qlchuoiphongtro/config/SwaggerConfig.java
@@ -1,19 +1,22 @@
 package com.trithienviet.qlchuoiphongtro.config;
 
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenApi(){
+        final String securitySchemeName = "Manager Room Application";
+
         Info info = new Info()
             .title("Manager Room Application")
             .description("Backend APIs for Manager Room app")
@@ -26,6 +29,14 @@ public class SwaggerConfig {
             .externalDocs(new ExternalDocumentation()
                 .description("Manager Room App Documentation")
                 .url("http://localhost:8080/swagger-ui/index.html")
-            );
+            )
+            // BƯỚC QUAN TRỌNG: Thêm cấu hình Security ở đây
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components()
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                    .name(securitySchemeName)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
     }
 }
