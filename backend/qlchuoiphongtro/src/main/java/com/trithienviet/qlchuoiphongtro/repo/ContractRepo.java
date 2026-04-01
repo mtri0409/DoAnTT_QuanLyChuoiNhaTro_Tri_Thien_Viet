14d51b480d5ecd5c31907a6c56976134e4b743ef
tranviet <tranvanviet07012005@gmail.com>
2026-04-01 14:34:41 +0700

    update ContractRepo queries

--- backend/qlchuoiphongtro/src/main/java/com/trithienviet/qlchuoiphongtro/repo/ContractRepo.java
+++ backend/qlchuoiphongtro/src/main/java/com/trithienviet/qlchuoiphongtro/repo/ContractRepo.java
@@ -1,13 +1,25 @@
 package com.trithienviet.qlchuoiphongtro.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.trithienviet.qlchuoiphongtro.entity.Contract;
+import com.trithienviet.qlchuoiphongtro.entity.ContractStatus;
 
 @Repository
 public interface ContractRepo extends JpaRepository<Contract, Long> {
+    boolean existsByRoom_RoomIdAndStatusAndIsDeletedFalse(
+            Long roomId,
+            ContractStatus status);
 
-    boolean existsByRoom_RoomIdAndStatus(Long roomId, String status);
+    List<Contract> findByStatusAndIsDeletedFalse(ContractStatus status);
 
+    List<Contract> findByStatusInAndIsDeletedFalse(
+            List<ContractStatus> statuses);
+
+    boolean existsByRoom_RoomIdAndStatusInAndIsDeletedFalse(
+            Long roomId,
+            List<ContractStatus> statuses);
 }
