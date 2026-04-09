e884724dbc2b70fc1fa97f816a21690fce0fcaee
tranviet <tranvanviet07012005@gmail.com>
2026-04-09 23:01:01 +0700

    chore(route): update routes for payment and invoice pages

--- frontend/QuanLyNhaTro_Tri_Thien_Viet_Tenant/src/route/route.js
+++ frontend/QuanLyNhaTro_Tri_Thien_Viet_Tenant/src/route/route.js
@@ -1,4 +1,7 @@
 import Home from "../pages/Home";
+import InvoiceDetail from "../pages/invoice/InvoiceDetail";
+import ListInvoice from "../pages/invoice/ListInvoice";
+import PaymentPage from "../pages/payment/PaymentPage";
 import ChangePassword from "../pages/profile/ChangePassword";
 import CreateVehicle from "../pages/profile/CreateVehicleProfile";
 import VehicleGridLayout from "../pages/profile/ManagerVehicle";
@@ -6,14 +9,16 @@ import ProfileDetail from "../pages/profile/ProfileDetail";
 import ProfileUpdate from "../pages/profile/UpdateProfile";
 import UpdateVehicle from "../pages/profile/UpdateVehicleProfile";
 
-
 const AdminRoute = [
   { path: "/", component: Home },
-  {path: "user/profile/:id",component:ProfileDetail},
-  {path: "user/profile/:id/update",component:ProfileUpdate},
-  {path: "user/change-password",component:ChangePassword},
-  {path: "user/manager-vehicle/:id",component:VehicleGridLayout},
-  {path: "user/vehicle/:id/create",component:CreateVehicle},
-  {path: "user/vehicle/:id/update",component:UpdateVehicle}
+  { path: "user/profile/:id", component: ProfileDetail },
+  { path: "user/profile/:id/update", component: ProfileUpdate },
+  { path: "user/change-password", component: ChangePassword },
+  { path: "user/manager-vehicle/:id", component: VehicleGridLayout },
+  { path: "user/vehicle/:id/create", component: CreateVehicle },
+  { path: "user/vehicle/:id/update", component: UpdateVehicle },
+  { path: "user/bills", component: ListInvoice },
+  { path: "user/bills/:invoiceId", component: InvoiceDetail },
+  { path: "payment/:invoiceId", component: PaymentPage },
 ];
-export default AdminRoute;            
+export default AdminRoute;
