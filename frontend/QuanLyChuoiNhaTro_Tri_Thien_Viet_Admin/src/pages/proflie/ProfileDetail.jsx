5d13c32819d7172b77b3394342d392c83debb489
minhtri <minhtri05k47@gmail.com>
2026-05-28 21:31:29 +0700

    re-fix route

--- frontend/QuanLyChuoiNhaTro_Tri_Thien_Viet_Admin/src/pages/proflie/ProfileDetail.jsx
+++ frontend/QuanLyChuoiNhaTro_Tri_Thien_Viet_Admin/src/pages/proflie/ProfileDetail.jsx
@@ -208,7 +208,7 @@ const ProfileDetail = () => {
           </div>
         </div>
         <button
-          onClick={() => navigate(`/profiles/${id}/update`)}
+          onClick={() => navigate(`/profile/${id}/update`)}
           className="btn btn-primary d-flex align-items-center gap-2 shadow-sm px-4"
         >
           <FaEdit /> Chỉnh sửa hồ sơ
