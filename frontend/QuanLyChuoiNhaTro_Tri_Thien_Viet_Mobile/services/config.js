aa0ec0f82eb7708a492c1309c2cd67bb931f9113
tranviet <tranvanviet07012005@gmail.com>
2026-06-13 15:28:01 +0700

    config ip

--- frontend/QuanLyChuoiNhaTro_Tri_Thien_Viet_Mobile/services/config.js
+++ frontend/QuanLyChuoiNhaTro_Tri_Thien_Viet_Mobile/services/config.js
@@ -4,3 +4,5 @@ export const API_HOST = window.location.hostname;
 export const API_PORT = "8080";
 export const apiURL = `http://${API_HOST}:${API_PORT}/api/v1/`;
 export const imgURL = `http://${API_HOST}:${API_PORT}`;
+
+console.log("API URL:", API_HOST);
